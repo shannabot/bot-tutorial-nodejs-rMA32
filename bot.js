c88497ae6fdb462dca7778ee44603b474c99adf2
shannabot <sej5ad@virginia.edu>
2016-03-27 23:26:46 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/cool guy/;  botRegexDL = /^\/DDL/i;botRegexSalt = /^\/salt/;botRegexRules = /^\/rules/
-      botRegexAll = /^((?!why).)*$/gm;
+      botRegexAll = /^((?!ERROR SYSTEM FAILURE).)*$/gm;
       botRegexAd=/^\/advance/;botRegexGTA = /^\/gta/; botRegexSC = /^\/SDL/i; botODB = /(.*\s+)(.*odb)(\s+.*)/i; botDuck = /^\/duck/;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexSh = /^\/shrug/; botRegexWk = /^\/users/; botRegexCC = /^\/cc/;
       botRegexSiege = /^\/siege/
@@ -17,7 +17,7 @@ function respond() {
                 "MIA","BUF","SF","WAS","NYJ","TB"]
   if(request.text && botRegexAll.test(request.text) &&(request.name.indexOf("Tailor")>-1 || request.name.indexOf("Rebecca")>-1)) {
     this.res.writeHead(200);
-    postMessage("why");
+    postMessage("ERROR SYSTEM FAILURE");
     this.res.end();
   }
   else if(request.text && botRegexDL.test(request.text)) {
